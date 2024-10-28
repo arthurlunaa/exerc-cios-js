4b366db22deceec54c809531c5ce08695443a5fe
arthurlunaa <henriquearthur192@gmail.com>
2024-10-28 13:47:22 -0300

    create: for-exercicio

---
+++ for.js
@@ -0,0 +1,89 @@
+//Exercício 1: Imprimir números de 1 a 10
+
+//Crie um loop que imprima os números de 1 a 10.
+//for(i = 1; i <=10; i ++ ){
+  //    console.log(i)
+//}
+
+//Exercício 2: Imprimir números pares entre 1 e 20
+//Crie um loop que imprima apenas os números pares de 1 a 20.
+
+//for (i =1; i<=20; i++  ) {
+  //  if(i% 2 ===0)
+    //console.log(i,'par')    
+//}
+
+//Exercício 3: Somar os números de 1 a 100
+//Crie um loop que calcule a soma de
+//todos os números de 1 a 100.
+
+//let num = 0;
+//for (let i = 1; i <= 100; i++) {
+  //num += num;
+  //console.log('somando 1 + 1 ate o a 100' + num)
+//}
+
+//Exercício 4: Imprimir os elementos de um array
+//Dado um array de frutas, use um loop para imprimir cada fruta.
+
+//const frutas =  [{
+ //   name:'Banana'   
+//},{
+  //  name:'Morango'
+//},
+//{name:'Uva'}]
+//for (let fruta of frutas) {
+ //   console.log(fruta.name)
+//}
+
+//Exercício 5: Multiplicar os números de 1 a 10 por 2
+//Crie um loop que imprima o resultado da multiplicação de cada número de 1 a 10 por 2.
+
+//for (i =1; i<=10; i++  ) {  
+    //console.log( i*2)
+//}
+
+//Exercício 6: Imprimir números de 10 a 1 (contagem regressiva)
+//Crie um loop que imprima os números de 10 até 1.
+
+//for(i = 0; i <=10; i -1 ){
+//       console.log(i)
+//}
+
+//Exercício 7: Encontrar o maior número em um array
+//Dado um array de números, use um loop para encontrar e imprimir o maior número.
+
+//let numeros = [10, 5, 8, 20, 3, 85];
+//let maiorNumero = numeros[0]; // Começamos assumindo que o primeiro número é o maior
+//for (let i = 1; i < numeros.length; i++) {
+//    if (numeros[i] > maiorNumero) {
+//        maiorNumero = numeros[i]; // Atualiza o maior número encontrado
+//    }
+//}
+
+//Exercício 8: Contar números ímpares entre 1 e 50
+//Crie um loop que conte quantos números ímpares existem entre 1 e 50 e imprima o resultado.
+
+//let total = 0
+//for (let i = 1; i <= 50; i += 2) {
+ // console.log(i);
+  //total++;
+//}
+//console.log(`Existem um total de números ímpares de. `+ total);
+
+//Exercício 9: Imprimir a tabuada de um número
+//Dado um número, use um loop para imprimir a tabuada desse número (de 1 a 10).
+//let numero = 1;
+//for (let i = 1; i <= 10; i++) {
+  //console.log(numero + " x " + i + " = " + (numero * i));
+//}
+
+//Exercício 10: Reverter uma string
+//Dada uma string, use um loop para imprimi-la de trás para frente.
+
+//let string = "amor"
+//let reverte = ""
+//for (let i = string.length - 1; i >= 0; i--) {
+//   reverte += string[i]
+ //  console.log(reverte);
+//}
